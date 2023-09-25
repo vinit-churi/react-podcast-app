d96d0be260449eb7c38da18e2a8044e2711e1d25
Vinit Churi <vinitchuri@hotmail.com>
2023-09-25 16:51:44 +0530

    setup the podcastApi layer

--- src/app/features/podcastApi.js
+++ src/app/features/podcastApi.js
@@ -1,21 +1,52 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { db } from "@src/firebase";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+  updateDoc,
+  arrayRemove,
+  arrayUnion,
+} from "firebase/firestore";
 export const podcastApi = createApi({
   reducerPath: "podcast",
   baseQuery: fakeBaseQuery(),
   endpoints: (builder) => ({
     createPodcast: builder.mutation({
-      queryFn: async (userId) => {
-        console.log(userId);
+      queryFn: async (userId, data) => {
+        try {
+          const docData = {
+            createdBy: userId,
+            ...data,
+          };
+          const docRef = await addDoc(collection(db, "podcasts"), docData);
+          console.log(docRef, "this is the docRef");
+        } catch (error) {
+          return { error: error.message };
+        }
       },
       transformResponse: (response) => {
         console.log(response, "this is the response");
         return response.data;
       },
+      invalidatesTags: ["Podcasts"],
     }),
     addEpisodeToPodcast: builder.mutation({
-      queryFn: async (userId, podcastId) => {
-        console.log(userId, podcastId);
+      queryFn: async (userId, data) => {
+        try {
+          const docData = {
+            createdBy: userId,
+            ...data,
+          };
+          const docRef = await addDoc(collection(db, "episodes"), docData);
+          console.log(docRef, "this is the docRef");
+        } catch (error) {
+          return { error: error.message };
+        }
       },
       transformResponse: (response) => {
         console.log(response, "this is the response");
@@ -24,7 +55,18 @@ export const podcastApi = createApi({
     }),
     getPodcast: builder.query({
       queryFn: async (podcastId) => {
-        console.log(podcastId);
+        try {
+          console.log(podcastId);
+          const docRef = doc(db, "podcasts", podcastId);
+          const docSnap = await getDoc(docRef);
+          console.log(docSnap, "this is the docSnap");
+          if (docSnap.exists()) {
+            console.log(docSnap.data(), "this is the docSnap data");
+            return docSnap.data();
+          }
+        } catch (error) {
+          return { error: error.message };
+        }
       },
       transformResponse: (response) => {
         console.log(response, "this is the response");
@@ -33,7 +75,18 @@ export const podcastApi = createApi({
     }),
     getUserPodcasts: builder.query({
       queryFn: async (userId) => {
-        console.log(userId);
+        try {
+          const docRef = collection(db, "podcasts");
+          const podcastQuery = query(docRef, where("createdBy", "==", userId));
+          const docSnap = await getDoc(podcastQuery);
+          console.log(docSnap, "this is the docSnap");
+          if (docSnap.exists()) {
+            console.log(docSnap.data(), "this is the docSnap data");
+            return docSnap.data();
+          }
+        } catch (error) {
+          return { error: error.message };
+        }
       },
       transformResponse: (response) => {
         console.log(response, "this is the response");
@@ -42,11 +95,79 @@ export const podcastApi = createApi({
     }),
     getAllPodcasts: builder.query({
       queryFn: async () => {
-        console.log("get all podcasts");
+        try {
+          console.log("get all podcasts");
+          const docRef = collection(db, "podcasts");
+          const querySnapshot = await getDocs(docRef);
+          let result = [];
+          querySnapshot.forEach((doc) => {
+            result.push(doc.data());
+          });
+          return result;
+        } catch (error) {
+          return { error: error.message };
+        }
       },
       transformResponse: (response) => {
         console.log(response, "this is the response");
         return response.data;
+      },
+    }),
+    addToSubscription: builder.mutation({
+      queryFn: async (userId, podcastId) => {
+        try {
+          const docRef = doc(db, "users", userId);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            await updateDoc(docRef, {
+              subscriptions: arrayUnion(podcastId),
+            });
+          } else {
+            const docRef = doc(db, "users", userId);
+            await addDoc(docRef, {
+              subscriptions: [podcastId],
+            });
+          }
+        } catch (error) {
+          return { error: error.message };
+        }
+      },
+      transformResponse: (response) => {
+        console.log(response, "this is the response");
+        return response.data;
+      },
+    }),
+    removePodcastFromSubscription: builder.mutation({
+      queryFn: async (userId, podcastId) => {
+        try {
+          const docRef = doc(db, "users", userId);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            await updateDoc(docRef, {
+              subscriptions: arrayRemove(podcastId),
+            });
+          } else {
+            return { error: "No subscriptions found" };
+          }
+        } catch (error) {
+          return { error: error.message };
+        }
+      },
+    }),
+    getUserSubscriptions: builder.query({
+      queryFn: async (userId) => {
+        try {
+          const docRef = doc(db, "users", userId);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            const docData = docSnap.data();
+            return docData.subscriptions;
+          } else {
+            return { error: "No subscriptions found" };
+          }
+        } catch (error) {
+          return { error: error.message };
+        }
       },
     }),
   }),
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCadkdqxL8djxTiymIS8mjwLvHIxvFwMfo",
   authDomain: "knowledge-hub-5aa9f.firebaseapp.com",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
